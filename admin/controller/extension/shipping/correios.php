042e17522e4e9653c1ff9a79a9541bf3bc404ac3
Valdeir Psr <valdeir.naval@gmail.com>
2020-04-29 00:18:30 -0300

    <feat>: Cria a estrutura base do painel admin.

---
+++ admin/controller/extension/shipping/correios.php
@@ -0,0 +1,57 @@
+<?php
+
+class ControllerExtensionShippingCorreios extends Controller
+{
+  private $error = [];
+
+  /**
+   * Cria a página de configuração e salva os dados quando
+   * receber uma requisição do tipo POST
+   *
+   * @return void
+   */
+  public function index()
+  {
+    $data = $this->load->language('extension/shipping/correios');
+
+    $this->document->setTitle($this->language->get('heading_title'));
+
+    $data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
+    $data['footer'] = $this->load->controller('common/footer');
+
+    $this->response->setOutput($this->load->view('extension/shipping/correios', $data));
+  }
+
+  /**
+   * Cria as tabelas necessárias para o funcionamento
+   * da extensão
+   *
+   * @return void
+   */
+  public function install()
+  {
+
+  }
+
+  /**
+   * Desinstala as tabelas e modificações do DB
+   * utilizadas pela extensão
+   *
+   * @return void
+   */
+  public function uninstall()
+  {
+
+  }
+
+  /**
+   * Valida os dados da requisição
+   *
+   * @return boolean
+   */
+  private function validate()
+  {
+
+  }
+}
---
+++ admin/model/extension/shipping/correios.php
@@ -0,0 +1,26 @@
+<?php
+
+class ModelExtensionShippingCorreios extends Model
+{
+  /**
+   * Cria as tabelas necessárias para o funcionamento
+   * da extensão
+   *
+   * @return void
+   */
+  public function createTables()
+  {
+
+  }
+
+  /**
+   * Remove as tabelas e modificações do DB
+   * utilizadas pela extensão
+   *
+   * @return void
+   */
+  public function dropTables()
+  {
+
+  }
+}
