fbbcee09ba7215dfbaf52a833ce89cefdae36e77
Valdeir Psr <valdeir.naval@gmail.com>
2020-04-28 23:56:59 -0300

    <feat>: Autoload

--- system/library/correios.php
+++ system/library/correios.php
@@ -1,9 +1,3 @@
 <?php
 
 require('correios/autoload.php');
-
-$box = new \ValdeirPsr\Correios\Box;
-$box->setWeight(PHP_INT_MAX);
-
-
-var_dump(number_format(PHP_INT_MAX, 2, ".", ""), $box->getWeight());
