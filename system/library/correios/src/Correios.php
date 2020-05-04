0f60d8270f8a69c7d5dacf9b73280da617a6aba3
Valdeir Psr <valdeir.naval@gmail.com>
2020-05-03 22:27:18 -0300

    <feat>: Adiciona função para capturar os preços
    Adiciona função para capturar os preços e prazos
    de entrega

--- system/library/correios/src/Correios.php
+++ system/library/correios/src/Correios.php
@@ -41,6 +41,11 @@ class Correios
   private $discount;
 
   /**
+   * Serviços do Correios
+   */
+  private $services = [];
+
+  /**
    * Initializa a classe
    * 
    * array['addresss']
@@ -62,11 +67,23 @@ class Correios
    * @param \ArrayObject $address
    * @param \ArrayObject $products
    */
-  public function __construct($address, $products)
+  public function __construct($services, $address, $products)
   {
-    if (empty($address['postcode'])) throw new \InvalidArgumentException('Postcode required');
+    if (!$this->validateServices($services)) {
+      throw new \InvalidArgumentException('Services invalid');
+    }
 
-    $this->validateProducts($products);
+    if (empty($address['postcode'])) {
+      throw new \InvalidArgumentException('Postcode required');
+    }
+
+    if (!$this->validateProducts($products)) {
+      throw new \InvalidArgumentException('Products invalid');
+    }
+
+    $this->services = $services;
+    $this->postcodeTo = $address['postcode'];
+    $this->products = $this->parseProducts($products);
   }
 
   /**
@@ -116,6 +133,79 @@ class Correios
   }
 
   /**
+   * Função responsável por realizar a pesquisa de preço e prazo
+   * no site do Correios
+   *
+   * @return Quote[]
+   */
+  public function getQuote()
+  {
+    $boxes = $this->buildBoxes();
+    $quotes = [];
+
+    foreach($this->services as $service) {
+      foreach($boxes as $box) {
+        $quotes[] = $service->getQuote($box);
+      }
+    }
+
+    return $quotes;
+  }
+
+  /**
+   * Transforma um produtos com quantidade superior a 1 em multiplos
+   * produtos com quantidade igual a 1
+   *
+   * @param array $products
+   * @return array
+   */
+  private function parseProducts($products)
+  {
+    foreach($products as $key => $product) {
+      if ($product['quantity'] > 1) {
+        for($count = 0; $count < $product['quantity']; $count++) {
+          $productCopy = $product;
+          $productCopy['quantity'] = 1;
+    
+          $products[] = $productCopy;
+          unset($products[$key]);
+        }
+      }
+    }
+
+    $products = array_values($products);
+
+    $postcodes = [];
+    
+    foreach($products as $key => $value) {
+      $postcodes[$key] = $value['shipping'];
+    }
+
+    array_multisort($products, $postcodes);
+
+    return $products;
+  }
+
+  /**
+   * Valida os serviços informados
+   *
+   * @param Service[] $services
+   * @return boolean
+   */
+  private function validateServices($services)
+  {
+    if (empty($services)) return false;
+
+    foreach($services as $service) {
+      if (!($service instanceof Service)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  /**
    * Verifica se os campos estão de acordo com os tipos (String, Float e Int)
    * 
    * @param array $products
@@ -151,14 +241,86 @@ class Correios
       'length_class_id' => ['filter' => FILTER_VALIDATE_INT, 'flags' => '']
     ];
 
-    foreach ($requiredKeysProducts as $key => $value) {
-      if (!array_key_exists($key, $products)) {
-        throw new \InvalidArgumentException("{$key} required");
-      }
-
-      if (!filter_var($products[$key], $value['filter'], $value['flags'])) {
-        throw new \UnexpectedValueException("{$key} = {$products[$key]} is invalid");
+    foreach ($products as $product) {
+      foreach ($requiredKeysProducts as $key => $value) {
+        if (!array_key_exists($key, $product)) {
+          throw new \InvalidArgumentException("{$key} required");
+        }
+  
+        if (!filter_var($product[$key], $value['filter'], $value['flags'])) {
+          throw new \UnexpectedValueException("{$key} = {$product[$key]} is invalid");
+        }
       }
     }
+
+    return true;
+  }
+
+  /**
+   * Função responsável por criar as caixas de acordo com o tamanho
+   * dos produtos.
+   *
+   * @return Box[]
+   */
+  private function buildBoxes()
+  {
+    $boxes = [];
+    $count = $boxId = 0;
+    $total = count($this->products) - 1;
+
+    foreach($this->services as $service) {
+      while ($count <= $total) {
+        if (!empty($boxes[$boxId]) && $boxes[$boxId]->getPostcodeFrom() != $this->products[$count]['shipping']) {
+          $boxId++;
+        }
+
+        if (!isset($boxes[$boxId])) {
+          $box = new Box();
+          $box->setPostcodeFrom($this->products[$count]['shipping']);
+          $box->setPostcodeTo($this->postcodeTo);
+
+          $boxes[$boxId] = $box;
+        }
+
+        $box = $boxes[$boxId];
+
+        /** Captura a dimensão do produto */
+        $pLength = $this->products[$count]['length'];
+        $pWidth = $this->products[$count]['width'];
+        $pHeight = $this->products[$count]['height'];
+        $pWeight = $this->products[$count]['weight'];
+        $pPrice = $this->products[$count]['price'];
+      
+        /** Captura a dimensão da caixa */
+        $bLength = (int)$box->getLength();
+        $bWidth = (int)$box->getWidth();
+        $bHeight = (int)$box->getHeight();
+        $bWeight = (int)$box->getWeight();
+        $bPrice = (int)$box->getTotalBox();
+      
+        /** Soma as dimensões do produto e da caixa */
+        $box->setLength($bLength + $pLength);
+        $box->setWidth($bWidth + $pWidth);
+        $box->setHeight($bHeight + $pHeight);
+        $box->setWeight($bWeight + $pWeight);
+        $box->setTotalBox($bPrice + $pPrice);
+
+        if ($service->validate($box, false)) {
+          $count++;
+        } elseif ($boxId > ($total + 1)) {
+          /** Evita loop infinito */
+          break;
+        } else {
+          /** Redefine as configurações da caixa */
+          $box->setLength($bLength);
+          $box->setWidth($bWidth);
+          $box->setHeight($bHeight);
+          $box->setTotalBox($bPrice);
+          $boxId++;
+        }
+      }
+    }
+
+    return $boxes;
   }
 }
