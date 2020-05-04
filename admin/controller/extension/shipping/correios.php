65b1630e285b25e22648bdd797b6551566d6797f
Valdeir Psr <valdeir.naval@gmail.com>
2020-05-04 10:04:14 -0300

    <style> Corrige identação

--- admin/controller/extension/shipping/correios.php
+++ admin/controller/extension/shipping/correios.php
@@ -17,7 +17,7 @@ class ControllerExtensionShippingCorreios extends Controller
     $this->document->setTitle($this->language->get('heading_title'));
 
     $data['header'] = $this->load->controller('common/header');
-		$data['column_left'] = $this->load->controller('common/column_left');
+    $data['column_left'] = $this->load->controller('common/column_left');
     $data['footer'] = $this->load->controller('common/footer');
 
     $this->response->setOutput($this->load->view('extension/shipping/correios', $data));
